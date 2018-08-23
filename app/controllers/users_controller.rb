2a3571e4008145c99c6cf22ce0380e949f4aa211
nataliya seryakova <nataliya.seryakova@gmail.com>
2018-08-23 13:03:17 -0400

    /user/rfid api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    user = User.find_by(rfid: params[:id])
+    render :json => user
   end
 
   # GET /users/new
